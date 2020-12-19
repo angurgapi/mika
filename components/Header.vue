9cfcdf9fb150777057391ecc835c8889cce0e2f5
angurgapi <sophiekovalsky@gmail.com>
2020-12-19 12:01:28 +0300

    created

---
+++ components/Header.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class='header'>
+    <div class="navpannel">
+        <div class="logo-block">
+          <img class='logo-img' src="img/nail_logo.png">
+          <div class="mikanails">mika nails</div>
+        </div>
+        <div class="navbar">
+          <ul>
+            <li class="nav-link"><a href='#about'>мастер</a></li>
+            <li class="nav-link"><a href='#service'>услуги</a></li>
+            <li class="nav-link"><a href='#contacts'>контакты</a></li>
+          </ul>
+        </div>
+      </div>
+    <div class="illustration">
+        <img src="img/ladies.png">
+      </div>
+ </div>
+</template>
+
+<style>
+
+@font-face{
+  font-family: 'Comfortaa';
+  src: local('Comfortaa-Medium'),
+  url(~assets/Comfortaa-Medium.ttf); 
+}
+
+
+a{
+
+}
+.header{
+  width: 100%;
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  background: linear-gradient(90deg, rgba(237, 220, 227, 1) 0%, rgba(237, 220, 227, .2) 50%, rgba(230, 185, 181,1) 100%);
+}
+.navpannel{
+/*  margin-top:20px;*/
+  height: 100px;
+  width: 100%;
+  padding: 0 1.5rem 0 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.logo-block{
+  width: 75px;
+  height: 75px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 3px solid rgba(0,0,0,.9);
+}
+.logo-img{
+  height: 30px;
+  width: 32px;
+}
+.mikanails{
+  font-family: 'Comfortaa';
+  text-align: center;
+  font-size: 18px;
+}
+.navbar{
+  width: 40%;
+  justify-content: space-around;
+}
+.navbar > ul{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  list-style: none;
+  padding-left: 0;
+}
+
+.nav-link a{
+  text-decoration: none;
+  color: rgba(0,0,0,.7);
+  font-family: 'Comfortaa';
+  font-size:24px;
+  font-weight: 500;
+  color: rgba(0,0,0,.7);
+}
+.nav-link a:hover, .nav-items a:active, .nav-items a.nuxt-link-active {
+    color:rgba(87, 23, 30, .5);
+  }
+
+@media(max-width: 500px){
+  .navbar > ul{
+    flex-direction: column;
+  }
+  .nav-link a{
+    font-size: 22px;
+  }
+}
+.illustration{
+  z-index:1000;
+  display: flex;
+  justify-content: center;
+
+}
+
+.illustration > img{
+  height: 15em;
+  width: 17em;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="main-block">
+    <Header />
+    <section id='about'>
+      <div class='about-me'>
+        <h2 class='section-headline'>обо мне</h2>
+        
+        <p>Меня зовут Мика. Я начала изучать искусство маникюра 8 лет назад, и на протяжении последних трех лет я с удовольствием посвящаю себя только этой сфере. Опыт я получала в Москве, а теперь готова радовать Вас маникюром в Оше. </p>        
+      </div>
+
+      <div class='works'>
+       <Carousel />
+     </div>
+    
+  </section>
+      <div class="discounts">
+      <h2 class='section-headline'>акции</h2>
+      <p>Поделись ссылкой на меня в Instagram и получи скидку 5% на маникюр или педикюр</p>
+    </div>
+</div>    
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Carousel from '@/components/Carousel'
+export default {
+  
+}
+</script>
+
+
+
+<style>
+.main-block{
+  max-width: 100%;
+  margin:0;
+}
+section{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.works{
+  width: 50%;
+}
+#about{
+  display: flex;
+  width:100%;
+  flex-direction: row;
+  height: 700px;  
+  justify-content: space-around;
+  
+  
+}
+.section-headline{
+  text-align: center;
+  font-weight: 600;
+  color: rgba(0,0,0,.7);
+}
+
+
+.about-me{
+  width:45%;
+}
+.about-me > ul{
+  
+  margin: auto;
+  list-style: none;
+  line-height: 35px;
+  text-align: center;
+}
+
+@media(max-width:500px){
+  
+  .illustration{
+  top:-310px;
+  }
+  .main-block{
+  top:-120px;
+  } 
+  .about-me{
+  width:90%;
+  }
+}
+
+@media(max-width: 810px){
+  #about{
+    flex-direction: column;
+    top:250px;
+  }
+}
+
+
+
+</style>
